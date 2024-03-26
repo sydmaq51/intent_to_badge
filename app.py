a10a46cb65c513be75ffbb67cd7e120a96b8e8a2
Krys Tyler <krys.tyler@snowflake.com>
2024-03-26 16:14:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -149,10 +149,10 @@ with tab4:
                                      'workshop_acro':['DWW','CMCW','DABW','DLKW','DNGW']})
             workshop_choice = st.selectbox("Choose Workshop/Badge want to enter/edit account info for:", options=badge_options, key=1)
             
-            if (workshop_results.iloc[0]['ACCOUNT_LOCATOR'] is not None):
+            if (workshops_results.iloc[0]['ACCOUNT_LOCATOR'] is not None):
                 st.session_state['new_acct_loc'] = workshop_results.iloc[0]['ACCOUNT_LOCATOR']
-            if (workshop_results.iloc[0]['ACCOUNT_ID'] is not None):    
-                st.session_state['new_acct_id'] = workshop_results.iloc[0]['ACCOUNT_ID']
+            if (workshops_results.iloc[0]['ACCOUNT_ID'] is not None):    
+                st.session_state['new_acct_id'] = workshops_results.iloc[0]['ACCOUNT_ID']
             
             with st.form("edit_acct_info"):
                 st.write("Edit Trial Account Info for " + workshop_choice['badge_name'])
