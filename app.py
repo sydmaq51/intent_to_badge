7c6555e5c84299e37ff96ab551bff89086c0ff4f
Krys Tyler <krys.tyler@snowflake.com>
2024-03-27 08:27:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ if find_my_uni_record:
     st.session_state['middle_name'] = ''
     st.session_state['family_name'] = ''
     st.session_state['badge_email'] = ''
+    st.sessoin_state['display_name'] = ''
     
     this_user_sql =  "select badge_given_name, badge_middle_name, badge_family_name, badge_email, display_format, display_name from UNI_USER_BADGENAME_BADGEEMAIL where UNI_ID=trim('" + uni_id + "') and UNI_UUID=trim('"+ uni_uuid +"')"
     this_user_df = session.sql(this_user_sql)
