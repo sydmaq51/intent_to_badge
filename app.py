c635d55f7b9124e3902445a46caa1f643bf5e228
Krys Tyler <krys.tyler@snowflake.com>
2024-03-27 12:03:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_user_profile_info():
    #start over with authentication and populating vars
    this_user_sql =  (f"select badge_given_name, badge_middle_name, badge_family_name, display_name, badge_email "
                      f"from UNI_USER_BADGENAME_BADGEEMAIL where UNI_ID=trim('{st.session_state.uni_id}') "
-                     f"and UNI_UUID=trim('{uni_uuid}')"
+                     f"and UNI_UUID=trim('{uni_uuid}')")
    this_user_df = session.sql(this_user_sql)
    user_results_pd_df = this_user_df.to_pandas()                          
    user_rows = user_results_pd_df.shape[0]
