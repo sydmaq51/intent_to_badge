74d2c1c851f986782fd6ab81e7c68daaae64da36
Krys Tyler <krys.tyler@snowflake.com>
2024-03-27 11:29:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def workshop_choice_changed():
    st.session_state['account_identifier'] = ''
    for_edits_df =  (f"select organization_id ||\'.\'|| account_name as ACCOUNT_IDENTIFIER, account_locator " 
                    f"from AMAZING.APP.USER_ACCOUNT_INFO_BY_COURSE where type = 'MAIN' "
-                   f"and UNI_ID= trim('{st.session_state.uni_id}') and UNI_UUID=trim('{st.session_state.uni_uuid}')" 
+                   f"and UNI_ID= trim('{st.session_state.uni_id}') and UNI_UUID=trim('{st.session_state.uni_uuid}') " 
                    f"and award_desc='{st.session_state.workshop_choice}'")
    st.write(for_edits_df)
    for_edits_df = session.sql(workshops_sql)
