6277932b71d015b5462d48e87f769e89be13af0b
Krys Tyler <krys.tyler@snowflake.com>
2024-03-27 18:03:41 -0700

    Update 3_view_trial_acct_info.py

--- pages/3_view_trial_acct_info.py
+++ pages/3_view_trial_acct_info.py
@@ -5,7 +5,7 @@ def get_user_workshop_acct_info():
    # get a table of all the entries this user has made
    workshops_sql =  (f"select award_desc, organization_id ||\'.\'|| account_name as ACCOUNT_IDENTIFIER, account_locator " 
                      f"from AMAZING.APP.USER_ACCOUNT_INFO_BY_COURSE where type = 'MAIN' and UNI_ID=trim('{st.session_state.uni_id}') " 
-                     f"and UNI_UUID=trim('{uni_uuid}')") 
+                     f"and UNI_UUID=trim('{st.session_state.uni_uuid}')") 
    workshops_df = session.sql(workshops_sql)
    workshops_results = workshops_df.to_pandas()
    workshops_rows = workshops_results.shape[0]
