682a822abcee439f5d18dcc604a178ff82f44dce
Krys Tyler <krys.tyler@snowflake.com>
2024-03-27 18:02:27 -0700

    Update 3_view_trial_acct_info.py

--- pages/3_view_trial_acct_info.py
+++ pages/3_view_trial_acct_info.py
@@ -1,6 +1,20 @@
 import streamlit as st
 import pandas as pd
 
+def get_user_workshop_acct_info():
+   # get a table of all the entries this user has made
+   workshops_sql =  (f"select award_desc, organization_id ||\'.\'|| account_name as ACCOUNT_IDENTIFIER, account_locator " 
+                     f"from AMAZING.APP.USER_ACCOUNT_INFO_BY_COURSE where type = 'MAIN' and UNI_ID=trim('{st.session_state.uni_id}') " 
+                     f"and UNI_UUID=trim('{uni_uuid}')") 
+   workshops_df = session.sql(workshops_sql)
+   workshops_results = workshops_df.to_pandas()
+   workshops_rows = workshops_results.shape[0]
+
+   # show the entries
+   if workshops_rows>=1:
+       st.write("You have entered account info for the following badge workshops:")
+       st.dataframe(workshops_results)
+
 cnx=st.connection("snowflake")
 session = cnx.session()
 
