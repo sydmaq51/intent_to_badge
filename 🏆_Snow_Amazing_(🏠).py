dc1c0e27e9b3b815151e5ebd7bfbaf2ab2ba274c
Krys Tyler <krys.tyler@snowflake.com>
2024-03-28 10:18:38 -0700

    Update and rename 🏆_Snow_Amazing_(Home).py to 🏆_Snow_Amazing_(🏠).py

--- 🏆_Snow_Amazing_(🏠).py
+++ 🏆_Snow_Amazing_(🏠).py
@@ -88,12 +88,12 @@ if st.session_state.auth_status == 'authed':
    st.markdown("**MIDDLE/ALTERNATE NAME:** "+ st.session_state.middle_name) 
    st.markdown("**FAMILY NAME:** " + st.session_state.family_name)
    st.markdown("**EMAIL:** " + st.session_state.badge_email)
-   if st.session_state.display_name != "PLEASE GO TO THE DISPLAY NAME TAB TO GENERATE A DISPLAY NAME FOR YOUR BADGE":
+   if st.session_state.display_name != "Please go to the FORMAT DISPLAY NAME page to generate a DISPLAY NAME for your badge(s).":
       st.markdown("**Name Will Display on Badge As:** " + st.session_state.display_name)
    else:
       md_str =  "**Name Will Display on Badge As:** :red[" + st.session_state.display_name + "]"       
       st.markdown(md_str)
       st.write("-----")
-      st.markdown("*If your display name has not been generated, or you would like to make changes to information, go to the Your Display Name page.*")
+      st.markdown("*If your display name has not been generated, or you would like to make changes to information, go to the FORMAT DISPLAY NAME page.*")
 else:
-   st.markdown(":red[Please sign in using your UNI_ID and UUID in the section above.]")
+   st.markdown(":red[Please sign in using your UNI_ID and UUID in the sidebar of the homepage.]")
