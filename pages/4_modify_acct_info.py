b0eb0217a49b2e6517870b4d798b64e06e9616ff
Krys Tyler <krys.tyler@snowflake.com>
2024-03-27 18:52:37 -0700

    Update 4_modify_acct_info.py

--- pages/4_modify_acct_info.py
+++ pages/4_modify_acct_info.py
@@ -29,12 +29,31 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 
 def workshop_chosen_changed():
-   st.session_state.editing_workshop = False
-   st.session_state['submit_new_account_info'] = False
-   st.session_state['account_locator'] = ''
-   st.session_state['account_identifier'] = ''
+   get_workshop_info()  
    st.session_state.aid_legit = False
    st.session_state.al_legit = False
+
+def get_workshop_info():   
+   for_edits_sql =  (f"select organization_id ||\'.\'|| account_name as ACCOUNT_IDENTIFIER, account_locator " 
+                   f"from AMAZING.APP.USER_ACCOUNT_INFO_BY_COURSE where type = 'MAIN' "
+                   f"and UNI_ID= trim('{st.session_state.uni_id}') and UNI_UUID=trim('{st.session_state.uni_uuid}') " 
+                   f"and award_desc='{st.session_state.workshop_choice}'")
+   # st.write(for_edits_sql)
+   for_edits_df = session.sql(for_edits_sql)
+   for_edits_pd_df = for_edits_df.to_pandas()
+   for_edits_pd_df_rows = for_edits_pd_df.shape[0]
+
+   # if the data row doesnt exist just seed it with blanks
+   if for_edits_pd_df_rows == 1:
+      if for_edits_pd_df['ACCOUNT_LOCATOR'].iloc[0] is not None:
+         st.session_state['account_locator'] = for_edits_pd_df['ACCOUNT_LOCATOR'].iloc[0] 
+      if for_edits_pd_df['ACCOUNT_IDENTIFIER'].iloc[0] is not None:
+         st.session_state['account_identifier'] = for_edits_pd_df['ACCOUNT_IDENTIFIER'].iloc[0]
+      st.write('Finished the function, boss!')   
+   elif for_edits_pd_df_rows == 0:
+      st.write('You have not previously entered account information for this workshop. Please add the information below.')
+   else:
+      st.write("there should only be 1 or zero rows.") 
 
 # drop list with option button for editing
 if st.session_state.auth_status == 'authed':
@@ -53,9 +72,10 @@ if st.session_state.auth_status == 'authed':
       if submit_button: 
          validate_acct_id(edited_acct_id)
          validate_acct_loc(edited_acct_loc)
-
-         st.session_state.edited_acct_id = edited_acct_id
-         st.session_state.edited_acct_loc = edited_acct_loc
+         if st.session_state.ai_legit == True and st.session_state.aid_legit==True:
+            st.session_state.edited_acct_id = edited_acct_id
+            st.session_state.edited_acct_loc = edited_acct_loc
+            session.call
 
 
 
