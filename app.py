d5c6cb148fd1d279d8ec456c5f785e1d793d2b28
Krys Tyler <krys.tyler@snowflake.com>
2024-03-27 17:15:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ st.set_page_config(
    page_icon= "🏆"
 )
 
+def initialize_user_info():
+   # session is open but not authed
+   st.session_state['auth_status'] = 'not_authed'
+   # all profile fields get set back to nothing
+   st.session_state['given_name'] = ''
+   st.session_state['middle_name'] = ''
+   st.session_state['family_name'] = ''
+   st.session_state['badge_email'] = ''
+   st.session_state['display_name'] = ''
+   # workshop/account fields are set back to nothing 
+   st.session_state['workshop_choice'] = '' 
+   st.session_state['account_locator'] = ''
+   st.session_state['account_identifier'] = ''
+   st.session_state['submit_new_account_info'] = False
+
+
 with st.sidebar:
    st.sidebar.header("User")
    uni_id = st.text_input('Enter your learn.snowflake.com UNI ID:')
