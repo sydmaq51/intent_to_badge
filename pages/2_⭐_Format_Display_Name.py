b3d4d13850dbf1da8344cd8c69a940ba14c4e110
Krys Tyler <krys.tyler@snowflake.com>
2024-04-10 07:03:07 -0700

    Update 2_⭐_Format_Display_Name.py

--- pages/2_⭐_Format_Display_Name.py
+++ pages/2_⭐_Format_Display_Name.py
@@ -37,8 +37,8 @@ st.subheader("Format the Display of Your Name for Your Badge(s)")
 
 if st.session_state.auth_status == 'authed':
    with st.form("display_formatting"):
-      display_option_1 = st.session_state.given_name.title() + " " + st.session_state.middle_name.capitalize() + " " + st.session_state.family_name.capitalize() #lazy do it for me
-      display_option_2 = st.session_state.given_name.capitalize() + " " + st.session_state.middle_name.capitalize() + " " + st.session_state.family_name #european w nobiliary
+      display_option_1 = st.session_state.given_name.title() + " " + st.session_state.middle_name.title() + " " + st.session_state.family_name.title() #lazy do it for me
+      display_option_2 = st.session_state.given_name.capitalize() + " " + st.session_state.middle_name + " " + st.session_state.family_name #european w nobiliary
       display_option_3 = st.session_state.family_name.upper() + " " + st.session_state.middle_name + " " + st.session_state.given_name.capitalize()  #east asian with alt script middle
       display_option_4 = st.session_state.family_name.upper() + " " +  st.session_state.given_name.capitalize() + " " + st.session_state.middle_name.capitalize() #east asian with alt script middle
       display_option_5 = st.session_state.given_name.capitalize() + " " +  st.session_state.middle_name.capitalize() + " " + st.session_state.family_name.upper() #ze french
