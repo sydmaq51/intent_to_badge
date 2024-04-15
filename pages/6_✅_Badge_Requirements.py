ee8a82074993542a4be3f72aa6d8981f628aaeda
Krys Tyler <krys.tyler@snowflake.com>
2024-04-15 09:06:52 -0700

    Update 6_✅_Badge_Requirements.py

--- pages/6_✅_Badge_Requirements.py
+++ pages/6_✅_Badge_Requirements.py
@@ -6,9 +6,11 @@ st.write("Please check the requirements listed on this page. We get SO MANY inqu
 
 st.write("This app is new as of April 9th, 2024. You may have used other methods to get badges in the past. The concepts are the same but the methods have changed.")
 st.markdown('----------')
-st.markdown(":white_check_mark: **STEP 1:** Tell us your name and email. Edit as needed. This is done on the :pencil2: page.") 
+st.markdown(":white_check_mark: **STEP 1:** Tell us your name and email.") 
+st.markdown("*Edit as needed. This is done on the :pencil2: page.*") 
 st.markdown('----------')
-st.markdown("**STEP 2:** Generate a Display Name. :green[This is NEW!] It gives you full control over how your name is displayed on any badge that issued. This is done on the :star: page.") 
+st.markdown(":white_check_mark:**STEP 2:** Generate a Display Name. :green[This is NEW!]") 
+st.markdown("*The Display Name feature gives you full control over how your name is displayed on any badge that issued. This is done on the :star: page.*") 
 st.markdown("")
 st.markdown("*Good news! You only have to do steps 1 and 2 the first time you use this app (unless your name or email changes). All future badges will have your correct information.*")
 st.markdown('------------')
