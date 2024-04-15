2d3eb2ccfe9ece951a750ee7e65622844b0d2f52
Krys Tyler <krys.tyler@snowflake.com>
2024-04-15 13:07:19 -0700

    Update 2_⭐_Format_Display_Name.py

--- pages/2_⭐_Format_Display_Name.py
+++ pages/2_⭐_Format_Display_Name.py
@@ -93,10 +93,12 @@ if st.session_state.auth_status == 'authed':
                 display_name_flag ='True'
             else: 
                 st.write('Choose a format for your name')
-
-            session.call('AMAZING.APP.UPDATE_BADGE_DISPLAYNAME_SP',st.session_state.uni_id, st.session_state.uni_uuid, display_format, edited_display_name)
-            get_user_profile_info()
-            st.success('Badge Display Name Updated', icon='🚀')
+                display_name_flag ='False'
+               
+            if display_name_flage =='True:
+               session.call('AMAZING.APP.UPDATE_BADGE_DISPLAYNAME_SP',st.session_state.uni_id, st.session_state.uni_uuid, display_format, edited_display_name)
+               get_user_profile_info()
+               st.success('Badge Display Name Updated', icon='🚀')
             
 
 else: # not authed
