f7c780d2202382e47ee8623e87abbc889d62a2b2
Krys Tyler <krys.tyler@snowflake.com>
2024-04-15 09:07:50 -0700

    Update 6_✅_Badge_Requirements.py

--- pages/6_✅_Badge_Requirements.py
+++ pages/6_✅_Badge_Requirements.py
@@ -14,14 +14,14 @@ st.markdown("*The Display Name feature gives you full control over how your name
 st.markdown("")
 st.markdown("*Good news! You only have to do steps 1 and 2 the first time you use this app (unless your name or email changes). All future badges will have your correct information.*")
 st.markdown('------------')
-st.markdown("**STEP 3:** For EVERY BADGE you hope to receive, you will need to see a row on the :chains: page.") 
+st.markdown(":white_check_mark:**STEP 3:** For EVERY BADGE you hope to receive, you will need to see a row on the :chains: page.") 
 st.markdown("To create or edit the info on the :chains: page, use the :link: page. Without this LINK established for each badge, DORA does not know who is doing the work so she cannot issue the badge.")         
 st.markdown("Every badge entry on the :chains: page should have both Account Locator and Account ID field completed.")
 st.markdown("")
 st.markdown("*Some older entries where you have already received your badge may have empty values. That is okay for older badges but for NEW badges, ALL columns MUST be complete.*")
 
 st.markdown('---------------------')
-st.markdown("**STEP 4:** For EVERY BADGE you hope to recieve, you need to complete every DORA check and see both PASSED and VALID for that test.") 
+st.markdown(":white_check_mark:**STEP 4:** For EVERY BADGE you hope to recieve, you need to complete every DORA check and see both PASSED and VALID for that test.") 
 st.markdown("A test that is PASSED but NOT VALID is a test that was changed by you before sending it to DORA.")
 
 
