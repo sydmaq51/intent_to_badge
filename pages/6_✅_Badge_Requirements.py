d7b9d96bd6be5a6f8c8e2e7ea32b3f384c431fe9
Krys Tyler <krys.tyler@snowflake.com>
2024-04-15 08:02:12 -0700

    Update and rename 6_Badge_Requirements.py to 6_✅_Badge_Requirements.py

---
+++ pages/6_✅_Badge_Requirements.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+
+st.subheader("Badge Requirements")
+st.write("Please check the requirements listed on this page. We get SO MANY inquiries that we should not get. This app should provide you with what you need to SELF-SERVICE any issues."
+
+         
