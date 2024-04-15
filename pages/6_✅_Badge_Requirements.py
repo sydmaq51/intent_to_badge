795fb40b8ac4823125016176dbae492257978ccb
Krys Tyler <krys.tyler@snowflake.com>
2024-04-15 08:06:43 -0700

    Update 6_✅_Badge_Requirements.py

--- pages/6_✅_Badge_Requirements.py
+++ pages/6_✅_Badge_Requirements.py
@@ -2,6 +2,11 @@
 import streamlit as st
 
 st.subheader("Badge Requirements")
-st.write("Please check the requirements listed on this page. We get SO MANY inquiries that we should not get. This app should provide you with what you need to SELF-SERVICE any issues."
+st.write("Please check the requirements listed on this page. We get SO MANY inquiries that we should not get. This app should provide you with what you need to SELF-SERVICE any issues.")
+
+st.write("This app is new as of April 9th, 2024. You may have used other methods to get badges in the past. The concepts are the same but the methods have changed.")
+
+st.write("STEP 1: Tell us your name and email. Edit as needed. This is done on the :pencil2: page.") 
+st.write("STEP 2: Generate a Display Name. This is NEW! It gives you full control over how your name is displayed on any badge that issued. This is done on the :star: page.") 
 
          
