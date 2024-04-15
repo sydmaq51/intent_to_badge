82fef308b33aa2c9098bc084328df450ddbda786
Krys Tyler <krys.tyler@snowflake.com>
2024-04-15 11:27:50 -0700

    Update 2_⭐_Format_Display_Name.py

--- pages/2_⭐_Format_Display_Name.py
+++ pages/2_⭐_Format_Display_Name.py
@@ -18,7 +18,7 @@ def get_user_profile_info():
       # 1 row found means the UNI_ID is legit and can be used to look up other information
       # all user vars need to be checked to make sure they aren't empty before we set session vars
       
-      st.dataframe(user_results_pd.df)   
+      st.dataframe(user_results_pd_df)   
 
          
       if user_results_pd_df['BADGE_GIVEN_NAME'].iloc[0] is not None:
