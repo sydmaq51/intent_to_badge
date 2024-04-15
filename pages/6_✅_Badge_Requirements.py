2dfc4d4eb7c4c4e77b94f0ebb7069ec0ba3ced9c
Krys Tyler <krys.tyler@snowflake.com>
2024-04-15 08:59:33 -0700

    Update 6_✅_Badge_Requirements.py

--- pages/6_✅_Badge_Requirements.py
+++ pages/6_✅_Badge_Requirements.py
@@ -5,17 +5,13 @@ st.subheader("Badge Requirements")
 st.write("Please check the requirements listed on this page. We get SO MANY inquiries that we should not get. This app should provide you with what you need to SELF-SERVICE any issues.")
 
 st.write("This app is new as of April 9th, 2024. You may have used other methods to get badges in the past. The concepts are the same but the methods have changed.")
-
 st.markdown('----------')
-
 st.markdown("**STEP 1:** Tell us your name and email. Edit as needed. This is done on the :pencil2: page.") 
 st.markdown('----------')
 st.markdown("**STEP 2:** Generate a Display Name. :green[This is NEW!] It gives you full control over how your name is displayed on any badge that issued. This is done on the :star: page.") 
 st.markdown("")
 st.markdown("*Good news! You only have to do steps 1 and 2 the first time you use this app (unless your name or email changes). All future badges will have your correct information.*")
-
 st.markdown('------------')
-
 st.markdown("**STEP 3:** For EVERY BADGE you hope to receive, you will need to see a row on the :chains: page.") 
 st.markdown("To create or edit the info on the :chains: page, use the :link: page. Without this LINK established for each badge, DORA does not know who is doing the work so she cannot issue the badge.")         
 st.markdown("Every badge entry on the :chains: page should have both Account Locator and Account ID field completed.")
