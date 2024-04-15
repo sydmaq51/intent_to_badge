c39e9fddf0110e4a9b97ebd6c142a5b3ba1b3e8f
Krys Tyler <krys.tyler@snowflake.com>
2024-04-15 11:40:55 -0700

    Update 2_⭐_Format_Display_Name.py

--- pages/2_⭐_Format_Display_Name.py
+++ pages/2_⭐_Format_Display_Name.py
@@ -44,7 +44,7 @@ st.write("You must generate a Display Name for your badge. If you do not, your b
 
 if st.session_state.auth_status == 'authed':
    st.markdown("--------")
-   if st.session_state.display_name_flage == False:
+   if st.session_state.display_name_flag == False:
       st.write('PLEASE CHOOSE A DISPLAY NAME')
    else:
       st.write("Your Display Name for Badges will be:")
