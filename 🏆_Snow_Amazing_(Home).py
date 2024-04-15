9ed20fa8c795404b52ee4b721d70eee04eeb22ef
Krys Tyler <krys.tyler@snowflake.com>
2024-04-15 13:27:15 -0700

    Update 🏆_Snow_Amazing_(Home).py

--- 🏆_Snow_Amazing_(Home).py
+++ 🏆_Snow_Amazing_(Home).py
@@ -20,6 +20,7 @@ def initialize_user_info():
    st.session_state['family_name'] = ''
    st.session_state['badge_email'] = ''
    st.session_state['display_name'] = ''
+   st.session_state['display_format'] = 0
    # workshop/account fields are set back to nothing 
    st.session_state['workshop_choice'] = '' 
    st.session_state['account_locator'] = ''
