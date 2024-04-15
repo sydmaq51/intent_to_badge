d1faf7054049a37b9ea57aaa98994519ec8f8f2a
Krys Tyler <krys.tyler@snowflake.com>
2024-04-15 08:58:12 -0700

    Update 6_✅_Badge_Requirements.py

--- pages/6_✅_Badge_Requirements.py
+++ pages/6_✅_Badge_Requirements.py
@@ -17,9 +17,10 @@ st.markdown("*Good news! You only have to do steps 1 and 2 the first time you us
 st.markdown('------------')
 
 st.markdown("**STEP 3:** For EVERY BADGE you hope to receive, you will need to see a row on the :chains: page.") 
-st.markdown("To create or edit the info on the :chains: page, use the :link: page. Without this LINK established for each badge, DORA does not know who is doing the work and sending her the tests.")         
+st.markdown("To create or edit the info on the :chains: page, use the :link: page. Without this LINK established for each badge, DORA does not know who is doing the work so she cannot issue the badge.")         
 st.markdown("Every badge entry on the :chains: page should have both Account Locator and Account ID field completed.")
-st.markdown("Some older entries where you have already received your badge may have empty values. That is okay for older badges but for new badges, all columns must be complete.")
+st.markdown("")
+st.markdown("*Some older entries where you have already received your badge may have empty values. That is okay for older badges but for new badges, all columns must be complete.*")
 
 st.markdown('---------------------')
 st.markdown("**STEP 4:** For EVERY BADGE you hope to recieve, you need to complete every DORA check and see both PASSED and VALID for that test.") 
