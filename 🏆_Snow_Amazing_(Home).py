a50b0829265860255f717ea4e38825bfbddb1753
Krys Tyler <krys.tyler@snowflake.com>
2024-04-15 13:50:22 -0700

    Update 🏆_Snow_Amazing_(Home).py

--- 🏆_Snow_Amazing_(Home).py
+++ 🏆_Snow_Amazing_(Home).py
@@ -62,7 +62,7 @@ def get_user_profile_info():
          st.session_state['display_name'] = user_results_pd_df['DISPLAY_NAME'].iloc[0]
       else:
          st.session_state['display_name'] = "Please go to the :star: page to generate a DISPLAY NAME for your badge(s)."
-         st.session_state['display_name_flag']='False'
+         st.session_state['display_name_flag'] = "False"
 
       if user_results_pd_df['display_format'] is not None:
          st.session_state['display_format'] = user_results_pd_df['DISPLAY_FORMAT'].iloc[0]
