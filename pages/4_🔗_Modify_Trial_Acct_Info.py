3d8257666129039b987bd3aea48f0990a0732220
Krys Tyler <krys.tyler@snowflake.com>
2024-04-17 07:44:24 -0700

    Update 4_🔗_Modify_Trial_Acct_Info.py

--- pages/4_🔗_Modify_Trial_Acct_Info.py
+++ pages/4_🔗_Modify_Trial_Acct_Info.py
@@ -83,7 +83,7 @@ if st.session_state.auth_status == 'authed':
       st.markdown("**Edit Trial Account Info for " + st.session_state.workshop_choice + "**")
       edited_acct_id = st.text_input("Enter Your Account Identifier as found in your Snowflake Account:", st.session_state.account_identifier, disabled=st.session_state.subform_toggle)
       edited_acct_loc = st.text_input("Enter Your Account Locator as found in your Snowflake Account:", st.session_state.account_locator, disabled=st.session_state.subform_toggle)
-      submit_button = st.form_submit_button("Update Trial Account Info")
+      submit_button = st.form_submit_button("Update Trial Account Info", disabled=st.session_state.subform_toggle)
 
       if submit_button: 
          if st.session_state.workshop_choice != '<Choose a Workshop>' and st.session_state.workshop_choice != ':red[NO WORKSHOP CHOSEN]':
