9144e540c128466873afefa100930fd57f92c444
Krys Tyler <krys.tyler@snowflake.com>
2024-04-22 12:06:25 -0700

    Update 6_✅_Badge_Requirements.py

--- pages/6_✅_Badge_Requirements.py
+++ pages/6_✅_Badge_Requirements.py
@@ -1,6 +1,10 @@
 
 import streamlit as st
 
+
+cnx=st.connection("snowflake")
+session = cnx.session()
+
 st.subheader(":white_check_mark: Badge Requirements")
 st.write("Please check the requirements listed on this page. We get SO MANY inquiries that we should not get. This app should provide you with what you need to SELF-SERVICE any issues.")
 
